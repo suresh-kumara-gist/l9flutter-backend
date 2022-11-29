da4bf049ec9dd98dec39655b19edba507b22bc93
suresh-kumara-gist <kumara.suresh25@gmail.com>
2022-11-29 17:06:59 +0100

    jwt dint work

--- app/config/app.php
+++ app/config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        'Tymon\JWTAuth\Providers\JWTAuthServiceProvider',
 
         /*
          * Application Service Providers...
@@ -229,7 +230,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAut,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory,
     ],
 
 ];
